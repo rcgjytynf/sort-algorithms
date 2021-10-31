0500cdbe45b039b4f2a821c1463d57ff6f064f7a
rcgjytynf <93493204+rcgjytynf@users.noreply.github.com>
2021-11-01 02:20:08 +0400

    Update fireheart.js

--- fireheart.js
+++ fireheart.js
@@ -3,33 +3,33 @@
 // The quick sort method is a classic algorithm, which is generally more efficient than the bubble method, and is very useful in sorting tabular data on the web client. The jscript implementation of the algorithm is given below.
 // Since the algorithm is very simple, no comments are added. 
 
-function quickSort( data, low ,high, bgreater) {
+function quickSort( data, low, high, bgreater) {
     var mid = data[ low ];
     var index = low;
     for( var i = low+1; i <= high; i++)  {
         var needswap = false;
         if( bgreater )
-            needswap = data[ i] <mid;
+            needswap = data[ i ] <mid;
         else
-            needswap = data[ i]> mid;
+            needswap = data[ i ]> mid;
         if( needswap) {
             index++;
             swap( data, i, index );
         }
     }
     swap( data, index, low );
-    if( index - 1 >  low) quickSort( data, low ,index-1 ,bgreater);
-    if( index + 1 < high) quickSort( data, index + 1, high ,bgreater);
+    if( index - 1 >  low) quickSort( data, low ,      index-1, bgreater);
+    if( index + 1 < high) quickSort( data, index + 1, high,    bgreater);
 }
 
-function swap( data_input, i ,low) {
+function swap( data_input, i, j) {
     var temp = data_input[ i ];
-    data_input[ i] = data_input[ low ];
-    data_input[low] = temp;
+    data_input[ i ] = data_input[ j ];
+    data_input[ j ] = temp;
 }
 
 //The test procedure is as follows:
 var data = new Array( 7 , -9, 38, 75, 6, -18, 204, 2, 0, 1, 42 );
 WScript.Echo( data );
-quickSort( data, 0, data.length-1 ,true);
+quickSort( data, 0, data.length-1, false);
 WScript.Echo( data ); 
